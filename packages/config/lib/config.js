16a2786df651ef42d7cb443ed4d6bfa88ecede69
Stefan Rimaila <stefan@rimaila.fi>
2021-07-13 20:15:35 +0300

    feat(config): update configuration

--- packages/config/lib/config.js
+++ packages/config/lib/config.js
@@ -1,5 +1,10 @@
-const out = (module.exports = {});
+const out = {};
 
 out.db = {
   database: 'etm12_homedata',
+  username: 'postgres',
+  password: null,
+  host: null,
 };
+
+module.exports = out;
